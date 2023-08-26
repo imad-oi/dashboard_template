c018f8fed43569e645fa8486deae53c229d4d050
imad-oi <oissafe.imad@gmail.com>
2023-08-26 18:18:41 +0100

    add darkmode

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ const control = "./src/assets/control.png";
 function App() {
   const [openSidebar, setopenSidebar] = useState(true);
 
+  const [isDarkMode, setisDarkMode] = useState(false);
+
   const menus = [
     { title: "Dashboard", src: "Chart_fill" },
     { title: "Inbox", src: "Chat" },
@@ -74,17 +76,42 @@ function App() {
           ))}
         </ul>
 
-        <div className={`flex justify-start items-center gap-x-2 duration-500
-        ${openSidebar && "bg-light-white p-4"}  h-16 w-60 rounded-md absolute bottom-4`}>
+        <div
+          className={`flex justify-start items-center gap-x-2 duration-500
+        ${
+          openSidebar && "bg-light-white p-4"
+        }  h-16 w-60 rounded-md absolute bottom-4`}
+        >
           <img src="./src/assets/logo.png" alt="profile" />
-          <div className={`text-gray-300 text-sm flex flex-col ${!openSidebar && "hidden"}`}>
+          <div
+            className={`text-gray-300 text-sm flex flex-col ${
+              !openSidebar && "hidden"
+            }`}
+          >
             <span className="font-semibold">Jhon Doe</span>
             <span>Jhon@Doe.com</span>
           </div>
         </div>
       </div>
 
-      <div className="p-7 text-2xl flex flex-col gap-y-4 font-semibold flex-1 h-screen overflow-y-scroll ">
+      <div
+        className={`p-7 text-2xl flex flex-col gap-y-4 font-semibold flex-1 h-screen overflow-y-scroll ${
+          !isDarkMode && "bg-black"
+        } `}
+      >
+        <div
+          className={`h-10 w-full flex justify-end ${
+            !isDarkMode && "bg-black"
+          }`}
+        >
+          <button
+            className="bg-blue-300 text-white text-sm font-semibold rounded-md px-4 py-2"
+            onClick={() => setisDarkMode((prev) => !prev)}
+          >
+            {isDarkMode ? "Light Mode" : "Dark Mode"}
+          </button>
+        </div>
+
         <div className="flex justify-between gap-x-4 ">
           <div className="bg-slate-200 w-1/2 border border-dark-purple border-dashed p-2 h-48 rounded-md "></div>
           <div className="bg-slate-200 w-1/2 border border-dark-purple border-dashed p-2 h-48 rounded-md "></div>
